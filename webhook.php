7775e35581b7979cc688cf6c4db1def0ae38eb59
DatDraggy <spamme@kieran.de>
2019-02-12 14:17:12 +0100

    per user, not per chat voting limit

--- webhook.php
+++ webhook.php
@@ -32,7 +32,7 @@ if (isset($data['callback_query'])) {
   if (stripos($callbackData, '|') !== false) {
     list($method, $feedbackMessageId, $confirm, $time) = explode('|', $callbackData);
     if ($method === 'vote') {
-      $timeouts = checkLastExecute($timeouts, 'vote', $chatType, $chatId);
+      $timeouts = checkLastExecute($timeouts, 'vote', $chatType, $senderUserId);
       if($timeouts === false){
         answerCallbackQuery($queryId);
         die();
