1c05818b1150c0d406ddf5391b76e0c832a6b30f
DatDraggy <bmxstreet@gmx.net>
2019-05-06 19:52:27 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -35,12 +35,12 @@ if (isset($data['callback_query'])) {
     list($method, $feedbackMessageId, $confirm, $time) = explode('|', $callbackData);
     if ($method === 'vote') {
       $timeouts = checkLastExecute($timeouts, 'vote', $chatType, $senderUserId);
-      mail($config['mail'], 'Debug', $timeouts);
+      mail($config['mail'], 'Debug', print_r($timeouts, true));
       if($timeouts === false){
         answerCallbackQuery($queryId);
         die();
       }
-      mail($config['mail'], 'Debug', $timeouts);
+      mail($config['mail'], 'Debug', print_r($timeouts, true));
       file_put_contents($config['timeoutsave'], json_encode($timeouts));
       $inlineQueryMessageId = $data['callback_query']['inline_message_id'];
       list($pollId, $status, $title, $pollText) = getPoll('', '', $inlineQueryMessageId);
